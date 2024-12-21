a19579a40440386a9ceb2d8c949f0d56923ab00f
Trisha-Annambhotla <144788501+Trisha-Annambhotla@users.noreply.github.com>
2024-12-21 21:37:47 +0530

    Add files via upload

---
+++ Hero.tsx
@@ -0,0 +1,28 @@
+
+const Hero=()=>{
+    return(
+        <div className="flex flex-col gap-20 text-white items-center mt-5">
+            <div>
+                <span className="font-press-start text-5xl text-shadow-glow text-yellow-200 ">IEEE-CS</span>
+            </div>
+            <div>
+                <h2 className="text-center font-press-start ">
+                Where innovation meets technology <br/>
+                We forge tech<br/> that transcends from ordinary <br/>
+                into realms of the unknown.
+                </h2>
+            </div>
+            <div>
+                <h1 className="font-press-start text-xl">&lt; Sign In with Google &gt;</h1>
+            </div>
+        </div>
+    );
+
+
+}
+
+export default Hero
+
+
+
+        
